62cb859fe870fed85ff018046b54ef880768ef67
Gullapalli Revanth Lakshmi Prasad <revanthlp2005@gmail.com>
2026-08-11 15:47:17 +0530

    Create initial FastAPI backend

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="AI Interview Coach")
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "AI Interview Coach API is running!"
+    }
